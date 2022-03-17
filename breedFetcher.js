c12db1c62edecde851163eee853d7948f3ffa900
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-17 14:38:05 -0400

    added errors for wrong url and wrong breed types

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,18 @@
-const request = require('request');
+const request = require("request");
 const argv = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${argv[0]}`, (error, response, body) => {
-console.log('Error: Breed not found, try another.', error);
-const data = JSON.parse(body);
-console.log(data);  
-console.log(typeof data);
-});
-g
+request(
+  `https://api.thecatapi.com/v1/breeds/search?q=${argv[0]}`,
+  (error, response, body) => {
+    if (error) {
+      console.log("Error: URL might be wrong", error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      console.log(`Breed not found: ${argv[0]}`);
+      return;
+    }
+    console.log(data[0].description);
+  }
+);
